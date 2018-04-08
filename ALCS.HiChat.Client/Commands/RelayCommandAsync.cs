1080edfae601f53efbb8b09da85f6276fb7cef47
Augusto Langer <augusto.langer@gmail.com>
2018-04-09 00:11:43 +0200

    Fixed text in toggle connection button

--- ALCS.HiChat.Client/Commands/RelayCommandAsync.cs
+++ ALCS.HiChat.Client/Commands/RelayCommandAsync.cs
@@ -9,22 +9,22 @@ namespace ALCS.HiChat.Client.Commands
 {
     public class RelayCommandAsync : ICommand
     {
-        private readonly Func<Task> _execute;
-        private readonly Predicate<object> _canExecute;
+        private readonly Func<Task> execute;
+        private readonly Predicate<object> canExecute;
         private bool isExecuting;
 
         public RelayCommandAsync(Func<Task> execute) : this(execute, null) { }
 
         public RelayCommandAsync(Func<Task> execute, Predicate<object> canExecute)
         {
-            _execute = execute;
-            _canExecute = canExecute;
+            this.execute = execute;
+            this.canExecute = canExecute;
         }
 
         public bool CanExecute(object parameter)
         {
-            if (!isExecuting && _canExecute == null) return true;
-            return (!isExecuting && _canExecute(parameter));
+            if (!isExecuting && canExecute == null) return true;
+            return (!isExecuting && canExecute(parameter));
         }
 
         public event EventHandler CanExecuteChanged
@@ -36,7 +36,7 @@ namespace ALCS.HiChat.Client.Commands
         public async void Execute(object parameter)
         {
             isExecuting = true;
-            try { await _execute(); }
+            try { await execute(); }
             finally { isExecuting = false; }
         }
     }
